c632afc61711b1ecc1eb96fab140e43320f97e7f
Sandra Bretones <sandrabretones@gmail.com>
2022-01-24 15:45:32 +0100

    portfolio 2.2

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,39 @@
 const navToogle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
-//const menuItem = doccument.querySelectorAll(".nav-menu__item")
+
 
 navToogle.addEventListener("click", () => {
    navMenu.classList.toggle("nav-menu__visible");
 });
 
-//menuItem.addEventListener("click", () => {
-  //  navMenu.classList.toggle("nav-menu__invisible");
-//});
+const menuItem = document.querySelector('.nav-menu__item');
+
+menuItem.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu__visible");
+}
+);
+const menuItem1 = document.querySelector('.nav-menu__item1');
+
+menuItem1.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu__visible");
+}
+);
+
+const menuItem2 = document.querySelector('.nav-menu__item2');
+
+menuItem2.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu__visible");
+}
+);
+
+const menuItem3 = document.querySelector('.nav-menu__item3');
+
+menuItem3.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu__visible");
+}
+);
+
+
 // Guardar estado imagen activa
 let imagenActiva = 0;
 
